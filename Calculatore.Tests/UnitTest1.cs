ff29abad89681472d05ac99ae7a1a22d309bd7f3
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-12-01 21:04:12 +0700

    proverka

---
+++ Calculatore.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Module16.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.True(100 == 100);
+    }
+
+
+}
